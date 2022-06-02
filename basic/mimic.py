2c8ccaebcf6c41844b6eda8e71ce5e19a1b68284
Mackhaster <mrshanelyse@hotmail.com>
2022-06-01 23:58:53 -0700

    Add mimic methods

--- basic/mimic.py
+++ basic/mimic.py
@@ -16,7 +16,7 @@ it into one giant string and split it once.
 
 Build a "mimic" dict that maps each word that appears in the file
 to a list of all the words that immediately follow that word in the file.
-The list of words can be be in any order and should include
+The list of words can be in any order and should include
 duplicates. So for example the key "and" might have the list
 ["then", "best", "then", "after", ...] listing
 all the words which came after "and" in the text.
@@ -46,26 +46,50 @@ import sys
 
 
 def mimic_dict(filename):
-  """Returns mimic dict mapping each word to list of words which follow it."""
-  # +++your code here+++
-  return
+    """Returns mimic dict mapping each word to list of words which follow it."""
+    f = open(filename, 'rU')
+
+    result_mimic_dict = {}
+    file_words = f.read().split()
+
+    # build the dict with the words and empty list
+    for word in file_words:
+        if word not in result_mimic_dict:
+            result_mimic_dict[word] = ['']
+
+    file_words_length = file_words.__len__()
+
+    for i in range(file_words_length):
+        if i < file_words_length - 1:
+            next_word = file_words[i + 1]
+            word_list = result_mimic_dict.get(file_words[i])
+            word_list.append(next_word)
+
+    f.close()
+    return result_mimic_dict
 
 
-def print_mimic(mimic_dict, word):
-  """Given mimic dict and start word, prints 200 random words."""
-  # +++your code here+++
-  return
+def print_mimic(mimic_dict_input, word):
+    """Given mimic dict and start word, prints 200 random words."""
+    print(word)
+    for i in range(199):
+        random_key = random.choice(list(mimic_dict_input))
+        random_list = mimic_dict_input[random_key]
+        random_word_from_list = random.choice(random_list)
+        print(random_word_from_list)
+
+    return None
 
 
 # Provided main(), calls mimic_dict() and mimic()
 def main():
-  if len(sys.argv) != 2:
-    print 'usage: ./mimic.py file-to-read'
-    sys.exit(1)
+    if len(sys.argv) != 2:
+        print('usage: ./mimic.py file-to-read')
+        sys.exit(1)
 
-  dict = mimic_dict(sys.argv[1])
-  print_mimic(dict, '')
+    result_dict = mimic_dict(sys.argv[1])
+    print_mimic(result_dict, '')
 
 
 if __name__ == '__main__':
-  main()
+    main()
